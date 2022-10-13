6d765082e0355b3b1f20472b3ffee1616077fdb4
davidevaliante <dav.valiante@gmail.com>
2022-10-13 17:40:29 +0200

    removed topads refer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,95 +1,127 @@
 import styled from 'styled-components'
 import Head from 'next/head'
-import React, { FunctionComponent, useEffect, useState } from 'react';
-import axios from 'axios';
-import { configuration } from '../configuration';
-import AquaClient from '../graphql/aquaClient';
-import { Streamer, StreamerBonus } from '../models/streamer';
-import Wrapper from '../components/Layouts/Wrapper';
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import axios from 'axios'
+import { configuration } from '../configuration'
+import AquaClient from '../graphql/aquaClient'
+import { Streamer, StreamerBonus } from '../models/streamer'
+import Wrapper from '../components/Layouts/Wrapper'
 import lowerCase from 'lodash/lowerCase'
-import BonusStripe from '../components/BonusStripe/BonusStripe';
-import VideoDiscalimer from '../components/VideoDisclaimer/VideoDisclaimer';
-import FullPageLoader from '../components/FullPageLoader';
-import Container from '../components/Layouts/Container';
+import BonusStripe from '../components/BonusStripe/BonusStripe'
+import VideoDiscalimer from '../components/VideoDisclaimer/VideoDisclaimer'
+import FullPageLoader from '../components/FullPageLoader'
+import Container from '../components/Layouts/Container'
 
 interface Props {
-  streamerData : Streamer
+	streamerData: Streamer
 }
 
-const index : FunctionComponent<Props> = ({streamerData}) => {
+const index: FunctionComponent<Props> = ({ streamerData }) => {
+	const [loading, setLoading] = useState(true)
+	const [country, setCountry] = useState<string>('')
+	useEffect(() => {
+		if (country !== '') getBonusList()
+	}, [country])
+	const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(
+		undefined
+	)
+	useEffect(() => {
+		console.log(bonuses)
+	}, [bonuses])
 
-    const [loading, setLoading] = useState(true)
-    const [country, setCountry] = useState<string>('')
-    useEffect(() => {
-      if(country !== '') getBonusList()
-    }, [country])
-    const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(undefined)
-    useEffect(() => {
-      console.log(bonuses)
-    }, [bonuses])
+	console.log(streamerData)
 
-    console.log(streamerData)
-   
-    useEffect(() => {
-        geoLocate()
-    }, [])
+	useEffect(() => {
+		geoLocate()
+	}, [])
 
-    const geoLocate = async () => {
-        setCountry('it')
-    }
+	const geoLocate = async () => {
+		setCountry('it')
+	}
 
-    const getBonusList = async () => {
-        let bonusForCountry = streamerData.countryBonusList.filter(it => it.label === country)
-        if(bonusForCountry.length == 0) bonusForCountry = streamerData.countryBonusList.filter(it => it.label === 'row')
+	const getBonusList = async () => {
+		let bonusForCountry = streamerData.countryBonusList.filter(
+			it => it.label === country
+		)
+		if (bonusForCountry.length == 0)
+			bonusForCountry = streamerData.countryBonusList.filter(
+				it => it.label === 'row'
+			)
 
-        const requests = bonusForCountry[0].bonuses.map(b =>  axios.get(`${configuration.api}/bonuses/${b.id}`))
+		const requests = bonusForCountry[0].bonuses.map(b =>
+			axios.get(`${configuration.api}/bonuses/${b.id}`)
+		)
 
-        const bList = await Promise.all(requests) 
+		const bList = await Promise.all(requests)
 
-        console.log(bList.map(r => r.data as StreamerBonus[]))
+		console.log(bList.map(r => r.data as StreamerBonus[]))
 
-        setBonuses(bList.map(r => r.data as StreamerBonus))
-        setLoading(false)
-    }
+		setBonuses(bList.map(r => r.data as StreamerBonus))
+		setLoading(false)
+	}
 
-    if(loading) return <FullPageLoader />
-    return (
-        <Wrapper>
-            <Container>
-                <div className='top-bar'>
-                    <img className='logo' src='/icons/app_icon.png' />
-                </div>
+	if (loading) return <FullPageLoader />
+	return (
+		<Wrapper>
+			<Container>
+				<div className='top-bar'>
+					<img className='logo' src='/icons/app_icon.png' />
+				</div>
 
-                <h1>Migliori casinò legali dove trovare questi giochi:</h1>
+				<h1>Migliori casinò legali dove trovare questi giochi:</h1>
 
-                {bonuses && bonuses.length > 2 && bonuses.map((bonus : StreamerBonus) => <BonusStripe key={`${bonus.name}`} bonus={bonus} countryCode={country} />)}
+				{bonuses &&
+					bonuses.length > 2 &&
+					bonuses.map((bonus: StreamerBonus) => (
+						<BonusStripe
+							key={`${bonus.name}`}
+							bonus={bonus}
+							countryCode={country}
+						/>
+					))}
 
-                {bonuses && bonuses.length <= 2 && streamerData.bonuses.map((bonus : StreamerBonus) => <BonusStripe key={`${bonus.name}`} bonus={bonus} countryCode={country} />)}
+				{bonuses &&
+					bonuses.length <= 2 &&
+					streamerData.bonuses.map((bonus: StreamerBonus) => (
+						<BonusStripe
+							key={`${bonus.name}`}
+							bonus={bonus}
+							countryCode={country}
+						/>
+					))}
 
-                <div style={{ padding: '1rem' }}>
-                    <VideoDiscalimer />
-                </div>
-                <div className='bottom'>
-                    <p style={{textAlign : 'center'}}>This service is provided by <a href='https://www.topaffiliation.com'>Top Affiliation</a></p>
-                </div>
-            </Container>
-        </Wrapper>
-    )
+				<div style={{ padding: '1rem' }}>
+					<VideoDiscalimer />
+				</div>
+				{process.env.REFER === 'true' && (
+					<div className='bottom'>
+						<p style={{ textAlign: 'center' }}>
+							This service is provided by{' '}
+							<a href='https://www.topaffiliation.com'>
+								Top Affiliation
+							</a>
+						</p>
+					</div>
+				)}
+			</Container>
+		</Wrapper>
+	)
 }
 
 export async function getServerSideProps({ query }) {
+	const pickedBonus = query.options
 
-  const pickedBonus = query.options
+	const aquaClient = new AquaClient()
 
-  const aquaClient = new AquaClient()
+	const streamer = await axios.get(
+		`${configuration.api}/streamers/${configuration.streamerId}`
+	)
 
-  const streamer = await axios.get(`${configuration.api}/streamers/${configuration.streamerId}`)
- 
-  return {
-      props: {
-          streamerData : streamer.data as Streamer,
-      }
-  }
+	return {
+		props: {
+			streamerData: streamer.data as Streamer,
+		},
+	}
 }
 
 export default index
